2db0260c6869688e53180b3b8c9e85e2409dcea3
rj <rraj411@gmail.com>
2020-03-02 16:55:19 +0530

    this is commitment

---
+++ videoApp/app/src/main/java/com/rishabhjaiswal/videoapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.rishabhjaiswal.videoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    VideoView video;
+    MediaController mc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        video= findViewById(R.id.videoView);
+        mc = new MediaController(this);
+        mc.setAnchorView(video);
+        video.setMediaController(mc);
+        video.setKeepScreenOn(true);
+        video.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.sample);
+        video.start();
+        video.requestFocus();
+
+    }
+}
